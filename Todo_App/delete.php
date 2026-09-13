c012587885e2df770eccb87e5042f18c2c57d382
elvisfmateo <elvisfmateo02@gmail.com>
2026-09-13 14:02:37 -0400

    Add files via upload

---
+++ Todo_App/delete.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once "db.php";
+
+session_start();
+
+if (!isset($_SESSION["user_id"])) {
+    header("Location: login.html");
+    exit();
+}
+
+if (!isset($_GET["id"])) {
+    header("Location: index.php");
+    exit();
+}
+
+$id = intval($_GET["id"]);
+$user_id = $_SESSION["user_id"];
+
+$sql = "DELETE FROM Tareas
+        WHERE id = ?
+        AND user_id = ?";
+
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("ii", $id, $user_id);
+
+$stmt->execute();
+
+header("Location: index.php");
+exit();
+
+?>
